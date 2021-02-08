47c1d74f91230d6aa2b2245cdf5cb789840cdda4
Andrew Chappell <chappand@gmail.com>
2021-02-08 12:47:56 -0500

    Formatted document.

--- Develop/index.js
+++ Develop/index.js
@@ -4,58 +4,58 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const promptUser = () =>
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What would you like your readme title to be?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please describe your application here.',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Please describe the installation process here.',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please add instrusctions for usage here.',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Describe you contributing guidelines here.',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Please enter instructions for testing here.',
-    },
-    {
-        type: 'input',
-        name: 'githubUN',
-        message: 'What is your GitHun username?',
-    },
-    {
-        type: 'input',
-        name: 'githubLink',
-        message: 'What is the link to your GitHub?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    },
-  ]);
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What would you like your readme title to be?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your application here.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please describe the installation process here.',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please add instrusctions for usage here.',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Describe you contributing guidelines here.',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please enter instructions for testing here.',
+        },
+        {
+            type: 'input',
+            name: 'githubUN',
+            message: 'What is your GitHun username?',
+        },
+        {
+            type: 'input',
+            name: 'githubLink',
+            message: 'What is the link to your GitHub?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ]);
 
 
 // TODO: Create a function to write README file
 const generateReadme = (userInput) =>
-`# ${userInput.title}
+    `# ${userInput.title}
 
 ## Description
 
@@ -96,10 +96,10 @@ If you have any further questions, please reach out to me via email at: ${userIn
 
 // Function call to initialize app
 promptUser()
-  .then((userInput) => fs.writeFile('README.md', generateReadme(userInput), (err) => {
-      if (err) {
-      console.log("There was an error, please try again!");
-    } else {
-        console.log('Successfully created Readme!');
-    }
+    .then((userInput) => fs.writeFile('README.md', generateReadme(userInput), (err) => {
+        if (err) {
+            console.log("There was an error, please try again!");
+        } else {
+            console.log('Successfully created Readme!');
+        }
     }));
